a4892bf78b4e972aa0c814747a5ddf743031bf2d
Lucasbon123 <est.bontempi.lucas@latecnicalf.com.ar>
2023-05-18 19:46:24 -0300

    controlador y ruteo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\tinderController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('tinder', App\Http\Controllers\tinderController::class);
